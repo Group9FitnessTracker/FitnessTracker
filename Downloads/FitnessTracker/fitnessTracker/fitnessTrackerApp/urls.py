752c71f383b777939ad86acfe0eed999a4388885
Beaumont Yin <beaumontyin@gmail.com>
2025-03-20 12:55:59 -0700

    Intial commit

---
+++ Downloads/FitnessTracker/fitnessTracker/fitnessTrackerApp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("/fitnessTrackerApp/5/", views.detail, name="detail"),
+    path("/fitnessTrackerApp/5/results", views.results, name="results"),
+    path("/fitnessTrackerApp/5/vote/", views.vote, name="vote"),
+]
