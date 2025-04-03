6bec69d2be1eb0efd75309a73aab1ba7040635ed
Beaumont Yin <beaumontyin@gmail.com>
2025-04-03 00:35:06 -0700

    Frontend: Create sign in page with bootstrap carousel and login form

--- Downloads/FitnessTracker/fitnessTracker/fitnessTrackerApp/urls.py
+++ Downloads/FitnessTracker/fitnessTracker/fitnessTrackerApp/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', views.home, name='home'),
+    path("index/", views.index, name="index"),
     path("/fitnessTrackerApp/5/", views.detail, name="detail"),
     path("/fitnessTrackerApp/5/results", views.results, name="results"),
     path("/fitnessTrackerApp/5/vote/", views.vote, name="vote"),
--- Downloads/FitnessTracker/fitnessTracker/fitnessTrackerApp/views.py
+++ Downloads/FitnessTracker/fitnessTracker/fitnessTrackerApp/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
+def home(request):
+    return render(request, 'fitnessTrackerApp/home.html')
 
 def index(request):
     return HttpResponse("Hello, world. You're at the default page.")
